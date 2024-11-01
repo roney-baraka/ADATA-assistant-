2697e90ce3679b8451031ac61f880e1fe11dec92
Ron <114308701+roney-baraka@users.noreply.github.com>
2024-11-01 06:07:18 +0300

    localhost

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
- 
+import os 
+import eel
+
+eel.init("www")
+os.system('open -a Safari "http://127.0.0.1:5500/index.html"')
+
+eel.start('index.html', mode=None, host='127.0.0.1', port=5500, block=True)
