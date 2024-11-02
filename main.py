6d6a4d3cce4a7b3399bd1532d72a26d59ac0731c
Ron <114308701+roney-baraka@users.noreply.github.com>
2024-11-02 13:26:50 +0300

    Text animation , Siri wave

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import os
 import eel
 
 eel.init("www")
-os.system('open -a Safari "http://127.0.0.1:5500/index.html"')
+os.system('open -a "Google Chrome" "http://127.0.0.1:5500/index.html"')
 
-eel.start('index.html', mode=None, host='127.0.0.1', port=5500, block=True)
+eel.start('index.html', mode=None, host='127.0.0.1', port=5500, block=True) 
--- www/main.js
+++ www/main.js
@@ -11,4 +11,33 @@ $(document).ready(function () {
          },
     });
 
+    const siriContainer = document.getElementById("siri-container");
+    if (siriContainer) {
+        // Initialize SiriWave
+        var siriWave = new SiriWave({
+            container: siriContainer,
+            width: 800,
+            height: 200,
+            style: "ios9",
+            amplitude: 1, 
+            speed: 0.3,   
+            autostart: true
+        });
+    };
+
+        // Siri message animation
+        $('.siri-message').textillate({
+            loop: true,
+            sync: true,
+            in: {
+                effect: "fadeInUp",
+                sync: true,
+            },
+            out: {
+                effect: "fadeOutUp",
+                sync: true,
+            },
+    
+        });
+
 });
