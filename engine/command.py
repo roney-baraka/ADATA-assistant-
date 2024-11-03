5b55440d28564045d661d67957a8a1f1cf9444f3
Ron <114308701+roney-baraka@users.noreply.github.com>
2024-11-03 15:19:46 +0300

    Speech recognitiion

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,7 @@
 import pyttsx3
+import speech_recognition as sr
+import eel
+import time 
 
 
 def speak(text):
@@ -6,6 +9,29 @@ def speak(text):
     engine.setProperty('rate', 174)
     engine.say(text)
     engine.runAndWait()    
+
+def takecommand():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening....')
+        eel.DisplayMessage('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('recognizing')
+        eel.DisplayMessage('recognizing....')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+        eel.DisplayMessage(query)
+        time.sleep(2)
+
+    except Exception as e:
+        return ""
+    
+    return query.lower()
 
 
 speak("Hi! Am ADATA")
