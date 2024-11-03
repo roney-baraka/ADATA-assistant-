c90833a7c8d03e5dd8aa3f4011df793d6842d5ce
Ron <114308701+roney-baraka@users.noreply.github.com>
2024-11-03 08:00:53 +0300

    A DATA start sound

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+# Playing assistant sound function
+def playAssistantSound():
+    music_dir = "/Users/ron/Documents/ADATA-assistant-/www/assets/audio/www_assets_audio_start_sound.mp3" 
+    playsound(music_dir)
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os 
 import eel
 
+from engine.features import *
+
 eel.init("www")
+
+playAssistantSound()
+
+
+
 os.system('open -a "Google Chrome" "http://127.0.0.1:5500/index.html"')
 
 eel.start('index.html', mode=None, host='127.0.0.1', port=5500, block=True) 
