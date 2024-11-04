1d13b2d9a66001f2c950f4573b23803260bc0e8f
Ron <114308701+roney-baraka@users.noreply.github.com>
2024-11-04 17:34:07 +0300

    Voice function

--- engine/command.py
+++ engine/command.py
@@ -16,15 +16,19 @@ def takecommand():
 
     with sr.Microphone() as source:
         print('listening....')
+        eel.DisplayMessage('listening....')
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source, 10, 6)
 
     try:
         print('recognizing')
+        eel.DisplayMessage('recognizing....')
         query = r.recognize_google(audio, language='en-in')
         print(f"user said: {query}")
+        eel.DisplayMessage(query)
         speak(query)
+        eel.ShowHood()
         time.sleep(2)
 
     except Exception as e:
