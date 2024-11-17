41f4eebc8fe6c943412d987685acd0278365c4fe
Ron <114308701+roney-baraka@users.noreply.github.com>
2024-11-18 02:31:17 +0300

    controller.js updates

--- www/controller.js
+++ www/controller.js
@@ -6,16 +6,25 @@ $(document).ready(function () {
         // Display Speak Message
         eel.expose(DisplayMessage)
         function DisplayMessage(message) {
-
-            $('.siri-message').text('message').fadeIn();
+            if ($('siri-message').length) {
+                $(this).text(message).fadeOut('fast', function() {
+                    $(this).text(message).fadeIn('fast');
+                });
+            } else {
+                console.error("Element '.siri-message' not found.") 
+            }
     
         }
 
             // Display hood
     eel.expose(ShowHood)
     function ShowHood() {
-        $("#Oval").attr("hidden", false);
-        $("#SiriWave").attr("hidden", true);
+        if ($("#Oval").length && $("#SiriWave").length) {
+            $("#Oval").attr("hidden", false);
+            $("#SiriWave").attr("hidden", true);
+        } else {
+            console.error("Elements '#Oval' or '#SiriWave' not found. ")
+        }
     }
 
 });
