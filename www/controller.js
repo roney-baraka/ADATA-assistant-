271cde49e6d18f1d59294ebf6a01dc28dd6d8821
Ron <114308701+roney-baraka@users.noreply.github.com>
2024-11-19 14:11:48 +0300

    controller and main.js changes

--- www/controller.js
+++ www/controller.js
@@ -6,22 +6,21 @@ $(document).ready(function () {
         // Display Speak Message
         eel.expose(DisplayMessage)
         function DisplayMessage(message) {
-            if ($('siri-message').length) {
-                $(this).text(message).fadeOut('fast', function() {
+            if ($('.siri-message').length){
+                $('.siri-message').fadeOut('fast', function () {
                     $(this).text(message).fadeIn('fast');
                 });
             } else {
-                console.error("Element '.siri-message' not found.") 
+                console.error("Element '.siri-message' not found.")
             }
-    
         }
-
             // Display hood
     eel.expose(ShowHood)
     function ShowHood() {
         if ($("#Oval").length && $("#SiriWave").length) {
             $("#Oval").attr("hidden", false);
             $("#SiriWave").attr("hidden", true);
+            console.log("ShowHood executed: #Oval shown, #SiriWave hidden.")
         } else {
             console.error("Elements '#Oval' or '#SiriWave' not found. ")
         }
--- www/main.js
+++ www/main.js
@@ -1,15 +1,29 @@
 $(document).ready(function () {
 
+
+if ($('.text').length) {
     $('.text').textillate({
-         loop: true,
-         sync: true,
-         in:{
+        loop: true,
+        sync: true,
+        in: {
             effect: "bounceIn",
-         },
-         out: {
-            effect: "bounceOut",
-         },
+        },
+        out: {
+            effect: "bounceOut"
+        },
     });
+}
+
+if ($('.siri-message').legth) {
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true,
+        },
+    });
+}
 
 
     // SiriWave initialization
@@ -25,6 +39,8 @@ $(document).ready(function () {
             speed: "0.30",   
             autostart: true
         });
+    } else {
+        console.error("#siri-container not found.");
     }
 
     //Siri message animation 
@@ -40,13 +56,19 @@ $(document).ready(function () {
     //Display message to Python and update UI
     eel.expose(DisplayMessage);
     function DisplayMessage(message) {
-        console.log(message);
-        $('.siri-message li:first').text(message);
-        $('.siri-message li:first').textillate('start');
+        console.log("DisplayMessage called with message", message);
+
+        if ($('.siri-message').length) {
+            $('.siri-message li:first').text(message);
+            $('.siri-message').textillate(start);
+        } else {
+            console.error("Element '.siri-message' not found.");
+        }
+
     }
 
     //Microphone button click event 
-    $("MicBtn").click(function (e) {
+    $("#MicBtn").click(function (e) {
         const micButton = $(this)
         micButton.prop("disabled", true);
         eel.playAssistantSound();
